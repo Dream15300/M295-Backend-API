4a19a6cc7441e8b4546f89a53e25bca0640fe2ca
IIacovelli <ivan.iacovelli@edu.gbssg.ch>
2025-12-09 09:48:37 +0100

    kleine Änderungen

--- dist/routes/authRoutes.js
+++ dist/routes/authRoutes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { login } from '../controllers/authController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 const router = Router();
-// POST /api/auth/login
+// GET /api/auth/login
 router.get('/login', login);
 // GET /api/auth/me
 router.get('/me', verifyToken);
