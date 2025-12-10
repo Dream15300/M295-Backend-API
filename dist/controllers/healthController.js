ec1784850192d1e0d3ae641e6beaf7342da073e6
Dream15300 <Ivan.iacovelli@bluewin.ch>
2025-12-10 09:48:16 +0100

    eingerichtet für meinen laptop. node_modules gelöscht, dann npm install

--- dist/controllers/healthController.js
+++
@@ -1,5 +0,0 @@
-// nur BSP, später löschen
-export function getHealth(_req, res) {
-    res.status(200).json({ status: 'ok' });
-}
-//# sourceMappingURL=healthController.js.map
--- dist/routes/health.js
+++
@@ -1,7 +0,0 @@
-// nur BSP, später löschen
-import { Router } from 'express';
-import { getHealth } from './../controllers/healthController.js';
-const router = Router();
-router.get('/', getHealth);
-export default router;
-//# sourceMappingURL=health.js.map
